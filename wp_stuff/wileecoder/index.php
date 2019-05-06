3c13000d1a4f9ac7147c9183381bf6ad9d525bef
luciens6669 <lstavenhagen@hotmail.com>
2019-05-06 17:44:48 -0600

    center text in header, create content-blog.php specifically for displaying the blog summary when in index.php

--- wp_stuff/wileecoder/index.php
+++ wp_stuff/wileecoder/index.php
@@ -38,10 +38,16 @@ get_header();
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
+				/* LS custom
+				* get the content-blog.php template part
+				* for the blog summary. It only shows
+				* the_excerpt.
+				* original line:
+				* get_template_part( 'template-parts/content', get_post_type() );
+				*/
+				get_template_part( 'template-parts/content', 'blog' );
+				
 			endwhile;
-
 			the_posts_navigation();
 
 		else :
